cb319162e6b09f38a04a46527668d79b055f0407
GFavaz <abashidze-giorgi@gzebi.com>
2021-01-17 17:23:03 +0400

    fizz game

--- src/fizz.kt
+++ src/fizz.kt
@@ -1,3 +1,14 @@
-class fizz {
+fun main() {
 
+    val array = IntArray(50) { it }
+    for (el in array) {
+        array[el] = array.indexOf(el)
+
+        when {
+            el % 15 == 0 -> println("Fizz Buzz")
+            el % 5 == 0 -> println("Buzz")
+            el % 3 == 0 -> println("Fizz")
+            else -> println(el)
+        }
+    }
 }
